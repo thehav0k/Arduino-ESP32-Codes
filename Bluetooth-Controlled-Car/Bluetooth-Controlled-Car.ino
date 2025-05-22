f5bec6130506be9c464faf8827a2ace6a783f3cf
Md. Asif Khan <20240659130asif@juniv.edu>
2025-05-22 14:59:17 +0600

    Update Bluetooth-Controlled-Car.ino

--- Bluetooth-Controlled-Car/Bluetooth-Controlled-Car.ino
+++ Bluetooth-Controlled-Car/Bluetooth-Controlled-Car.ino
@@ -7,18 +7,28 @@ BluetoothSerial SerialBT;
 #define IN3 18
 #define IN4 19
 
+// LED pins
+#define RED_LED 2
+#define GREEN_LED 15
+
 char command;
 
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32-Car");  // Bluetooth name
 
+  // Motor pins setup
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
+  // LED pins setup
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
   stopMotors();
+  turnOffLEDs();
   Serial.println("Bluetooth car ready. Connect and send commands.");
 }
 
@@ -36,6 +46,17 @@ void loop() {
       case 'S': stopMotors(); break;
     }
   }
+
+  // LED effects during movement
+  if (command == 'F' || command == 'B') {
+    // Blink both LEDs for forward/reverse
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, HIGH);
+    delay(200);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    delay(200);
+  }
 }
 
 // Motor movement functions
@@ -52,14 +73,24 @@ void reverse() {
 void turnRight() {
   digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
+  digitalWrite(RED_LED, HIGH);   // Red LED on for right turn
+  digitalWrite(GREEN_LED, LOW);
 }
 
 void turnLeft() {
   digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, HIGH); // Green LED on for left turn
 }
 
 void stopMotors() {
   digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
+  turnOffLEDs();
+}
+
+void turnOffLEDs() {
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
 }
